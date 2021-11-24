50005a98bf7288e1680576e1289c54e91adbef7f
dcaudill724 <dustincaudill724@gmail.com>
2021-11-23 19:48:21 -0500

    Initial commit

---
+++ AIProject.py
@@ -0,0 +1,10 @@
+from graphics import *
+
+def main():
+    win = GraphWin("AIProject", 800, 800) #initialize window
+    win.setBackground(color_rgb(0, 150, 50))
+    
+    win.getMouse()
+
+
+main()
